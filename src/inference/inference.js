0770288c055dd4f76e7d126e5741db20b07910bc
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2017-07-07 18:26:56 +0800

    Added crossreduce function

--- src/inference/inference.js
+++ src/inference/inference.js
@@ -1,7 +1,11 @@
 import { standardDeviation } from '../basic/basic';
+import { crossreduce } from '../util/crossreduce';
 
 function pairedTTest(numbers, twoTailed = true) {
-  return standardDeviation(numbers);
+  const diffs = crossreduce([[1, 2], [1, 2]], (acc, number) => acc + number, 0);
+  const diffSquareds = diffs.map(number => Math.pow(number, 2));
+
+  return diffSquareds;
 }
 
 export {
---
+++ src/util/crossreduce.js
@@ -0,0 +1,17 @@
+// TODO: Extract functions from this function
+
+export const crossreduce = (arrays, reducer) => {
+  const arrayLength = arrays[0].length;
+  const arrayCount = arrays.length;
+  const mapped = [];
+
+  for (let i = 0; i < arrayLength; i++) {
+    let slice = [];
+    for (let j = 0; j < arrayCount; j++) {
+      slice.push(arrays[j][i]);
+    }
+    mapped.push(slice.reduce(reducer));
+  }
+
+  return mapped;
+}
