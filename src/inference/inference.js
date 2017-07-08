47a581c7c44422427c481832cf7413a68f05f015
John Paul Ada <johnpaulada@Elie-Mini.local.gap>
2017-07-08 12:06:44 +0800

    Implemented paired t-test

--- src/inference/inference.js
+++ src/inference/inference.js
@@ -1,11 +1,21 @@
-import { standardDeviation } from '../basic/basic';
+import { sum, standardDeviation } from '../basic/basic';
 import { crossreduce } from '../util/crossreduce';
+import t_table from '../tables/t-table.js';
 
 function pairedTTest(numbers, twoTailed = true) {
-  const diffs = crossreduce([[1, 2], [1, 2]], (acc, number) => acc + number, 0);
+  const sampleSize = numbers[0].length;
+  const diffs = crossreduce(numbers, (acc, number) => acc - number);
   const diffSquareds = diffs.map(number => Math.pow(number, 2));
+  const diffSum = sum(diffs);
+  const diffSquaredSum = sum(diffSquareds);
+  const diffSumSquared = Math.pow(diffSum, 2);
+  const numerator = diffSum / sampleSize;
+  const rootNumerator = diffSquaredSum - diffSumSquared / sampleSize;
+  const rootDenominator = sampleSize * (sampleSize-1);
+  const denominator = Math.sqrt(rootNumerator / rootDenominator);
+  const t = numerator / denominator;
 
-  return diffSquareds;
+  return t;
 }
 
 export {
---
+++ src/inference/inference.test.js
@@ -0,0 +1,5 @@
+import { pairedTTest } from './inference.js';
+
+// test('Paired t-test should work', () => {
+//   expect(pairedTTest([[16, 3, 17, 3, 19, 15, 24, 23, 3, 12, 32], [32, 22, 23, 13, 20, 29, 11, 25, 13, 20, 30]])).toBe(6);
+// });
