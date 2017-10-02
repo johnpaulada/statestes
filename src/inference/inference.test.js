7cb64a4497ab5d74c0e825a80f2c014df05b5541
Murilo Dell Agnolo Garcia <dag.murilo@gmail.com>
2017-10-01 22:27:37 -0300

    Add pearson r correlation calculation

--- src/inference/inference.test.js
+++ src/inference/inference.test.js
@@ -1,1 +1,15 @@
-import { pairedTValue, independentTValue } from './inference.js'
+import { pairedTValue, independentTValue, pearsonR } from './inference.js'
+
+test('Pearson R small array', () => {
+    expect(pearsonR(
+        [1, 10],
+        [2, 1]
+    )).toBe(-1);
+})
+
+test('Pearson R big array', () => {
+    expect(pearsonR(
+        [1, 3, 4, 6, 9],
+        [2, 3.5, 5, 8, 7]
+    )).toBe(0.8732169121726039);
+})
